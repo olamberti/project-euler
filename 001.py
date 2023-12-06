0c15bbb1ef284784a44907c0b651c4c6ea0cfdb2
Oliver Lamberti <lamberti.oliver@gmail.com>
2023-12-06 20:57:47 +0100

    001 solution

--- 001.py
+++ 001.py
@@ -1,11 +1,7 @@
-# Sum of mulitples of 3:
-sum3 = sum([x for x in range(3, 1000, 3)])
-
-# Sum of mulitples of 5:
-sum5 = sum([x for x in range(5, 1000, 5)])
-
-# Sum of mulitples of 15:
-sum15 = sum([x for x in range(15, 1000, 15)])
+# Sum function for x
+def calc_sum(x):
+    n = 999 // x
+    return (x * n * (n + 1)) // 2
 
 # Add up the sums of 3 & 5 and subtract 15:
-print(sum3 + sum5 - sum15)
+print(calc_sum(3) + calc_sum(5) - calc_sum(15))
